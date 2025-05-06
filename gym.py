e309b9cf26ec7de97da994369f8cfad61415c331
svenusa <145644782+svenusa@users.noreply.github.com>
2025-05-06 14:35:41 +0300

    Push
    We ar genuisi

--- gym.py
+++ gym.py
@@ -12,59 +12,254 @@ logs.configure(background="lime")
 
 def aprekinat():
     abonements = abonements_var.get()
-    trenins = trenins_.get()
+    trenins = trenins_var.get()
     pakalpojums = pakalpojums_var.get()
+    rajons = rajons_var.get()
 
     if abonements == "Gada":
         abonements = 365
 
         if trenins == "Svaru zāle":
-            trenins = 5
+            trenins = 1.2
 
-            if pakalpojums == "Baseins":
-                    pakalpojums = 3
-            elif pakalpojums == "Sauna":
-                    pakalpojums = 2
-            elif pakalpojums == "Masāžas krēsls":
-                    pakalpojums = 1
-            elif pakalpojums == "-":
+            if rajons == "Ķengarags":
+                rajons = 0.1
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
                     pakalpojums = 0
 
+            if rajons == "Jēkabpils":
+                rajons = 0.3
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jūrmala":
+                rajons = 0.5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+        if trenins == "Grupas treniņš":
+            trenins = 1.4
+
+            if rajons == "Ķengarags":
+                rajons = 0.1
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jēkabpils":
+                rajons = 0.3
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jūrmala":
+                rajons = 0.5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+        if trenins == "Kardio":
+            trenins = 0.8
+
+            if rajons == "Ķengarags":
+                rajons = 0.1
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jūrmala":
+                rajons = 0.5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
 
     if abonements == "Mēneša":
         abonements = 31
 
         if trenins == "Svaru zāle":
-            trenins = 0.2
+            trenins = 1.3
 
-            if pakalpojums == "Baseins":
-                    pakalpojums = 3
-            elif pakalpojums == "Sauna":
-                    pakalpojums = 2
-            elif pakalpojums == "Masāžas krēsls":
-                    pakalpojums = 1
-            elif pakalpojums == "-":
+            if rajons == "Ķengarags":
+                rajons = 0.1
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
                     pakalpojums = 0
 
-
-
-            if rajons == "Jūrmala":
-                rajons = 5
+            if rajons == "Jēkabpils":
+                rajons = 0.3
                 
                 if pakalpojums == "Baseins":
-                    pakalpojums = 3
+                    pakalpojums = 0.3
                 elif pakalpojums == "Sauna":
-                    pakalpojums = 2
+                    pakalpojums = 0.2
                 elif pakalpojums == "Masāžas krēsls":
-                    pakalpojums = 1
-                elif pakalpojums == "-":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
                     pakalpojums = 0
 
-    if abonements == "Izmēģinājuma":
-        abonements = 1
+            if rajons == "Jūrmala":
+                rajons = 0.5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
 
         if trenins == "Grupas treniņš":
-            trenins = 5
+            trenins = 1.5
+
+            if rajons == "Ķengarags":
+                rajons = 0.1
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jēkabpils":
+                rajons = 0.3
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jūrmala":
+                rajons = 0.5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if trenins == "Kardio":
+                 trenins = 0.9
+
+            if rajons == "Ķengarags":
+                rajons = 0.1
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jēkabpils":
+                rajons = 0.3
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jūrmala":
+                rajons = 0.5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 0.3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 0.2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 0.1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+
+
+    if abonements == "Izmēģinājuma":
+            abonements = 1
+
+            if trenins == "Svaru zāle":
+                trenins = 5
 
             if rajons == "Ķengarags":
                 rajons = 1
@@ -75,7 +270,46 @@ def aprekinat():
                     pakalpojums = 2
                 elif pakalpojums == "Masāžas krēsls":
                     pakalpojums = 1
-                elif pakalpojums == "-":
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jēkabpils":
+                rajons = 5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jūrmala":
+                rajons = 5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if trenins == "Grupas treniņš":
+                trenins = 7
+
+            if rajons == "Ķengarags":
+                rajons = 1
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 1
+                elif pakalpojums == "Nav":
                     pakalpojums = 0
 
             if rajons == "Jēkabpils":
@@ -87,7 +321,7 @@ def aprekinat():
                     pakalpojums = 2
                 elif pakalpojums == "Masāžas krēsls":
                     pakalpojums = 1
-                elif pakalpojums == "-":
+                elif pakalpojums == "Nav":
                     pakalpojums = 0
 
             if rajons == "Jūrmala":
@@ -99,64 +333,94 @@ def aprekinat():
                     pakalpojums = 2
                 elif pakalpojums == "Masāžas krēsls":
                     pakalpojums = 1
-                elif pakalpojums == "-":
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if trenins == "Kardio":
+                 trenins = 4
+
+            if rajons == "Ķengarags":
+                rajons = 1
+
+                if pakalpojums == "Baseins":
+                    pakalpojums = 3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jēkabpils":
+                rajons = 3
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 1
+                elif pakalpojums == "Nav":
+                    pakalpojums = 0
+
+            if rajons == "Jūrmala":
+                rajons = 5
+                
+                if pakalpojums == "Baseins":
+                    pakalpojums = 3
+                elif pakalpojums == "Sauna":
+                    pakalpojums = 2
+                elif pakalpojums == "Masāžas krēsls":
+                    pakalpojums = 1
+                elif pakalpojums == "Nav":
                     pakalpojums = 0
 
 
+    kopējā_cena = abonements*(trenins + pakalpojums + rajons)
+    rez_label.config(text=f"Jūsu kopējā cena ir {kopējā_cena}€")
 
 
-
-
-
-
-
-
-
-
-    kopējā_cena = cena_dienā * nomas_dienas
-    rez_label.config(text=f"Jūsu kopējā cena ir {kopējā_cena}")
-
-
-dzinejs_var = StringVar()
-auto_markas_var = StringVar()
-nomas_dienas_var = StringVar()
-auto_veids_var = StringVar()
+abonements_var = StringVar()
+trenins_var = StringVar()
+pakalpojums_var = StringVar()
+rajons_var = StringVar()
 
 #auto markas izvēle
-ttk.Label(logs, text="Izvēlēties auto marku:").grid(row=1, column=0,padx=20,pady=20)
-auto_markas = ttk.Combobox(logs, textvariable = auto_markas_var)
-auto_markas["values"] = ("Volvo", "Nissan", "Mercedes")
-auto_markas.grid(column=1, row=1)
+ttk.Label(logs, text="Izvēlēties abonementa veidu:").grid(row=1, column=0,padx=20,pady=20)
+abonements = ttk.Combobox(logs, textvariable = abonements_var)
+abonements["values"] = ("Gada", "Mēneša", "Izmēģinājuma")
+abonements.grid(column=1, row=1)
 
-ttk.Label(logs, text="Izvēlēties dzinēju:").grid(row=2, column=0,padx=20,pady=20)
-dzinejs= ttk.Combobox(logs, textvariable = dzinejs_var)
-dzinejs["values"] = ("Dīzelis", "Koka", "Elektriskais")
-dzinejs.grid(column=1, row=2)
+ttk.Label(logs, text="Izvēlēties treniņa veidu:").grid(row=2, column=0,padx=20,pady=20)
+trenins= ttk.Combobox(logs, textvariable = trenins_var)
+trenins["values"] = ("Svaru zāle", "Grupas treniņš", "Kardio")
+trenins.grid(column=1, row=2)
 
-ttk.Label(logs, text="Izvēlēties auto veidu:").grid(row=3, column=0,padx=20,pady=20)
-auto_veids = ttk.Combobox(logs, textvariable = auto_veids_var)
-auto_veids["values"] = ("Traktors", "Vieglais", "Smagais")
-auto_veids.grid(column=1, row=3)
+ttk.Label(logs, text="Izvēlēties pakalpojumu:").grid(row=3, column=0,padx=20,pady=20)
+pakalpojums = ttk.Combobox(logs, textvariable = pakalpojums_var)
+pakalpojums["values"] = ("Baseins", "Sauna", "Masāžas krēsls", "Nav")
+pakalpojums.grid(column=1, row=3)
 
-ttk.Label(logs, text="Izvēlēties nomas dienu:").grid(row=4, column=0,padx=20,pady=20)
-nomas_dienas = tk.Entry(logs, textvariable = nomas_dienas_var)
-nomas_dienas.grid(column=1, row=4)
+ttk.Label(logs, text="Izvēlēties rajonu:").grid(row=4, column=0,padx=20,pady=20)
+rajons = ttk.Combobox(logs, textvariable = rajons_var)
+rajons["values"] = ("Jūrmala", "Jēkabpils", "Ķengarags")
+rajons.grid(column=1, row=4)
 
 
 def saglabatText():
-    auto_type = auto_veids.get()
-    auto_mark = auto_markas.get()
-    engine = dzinejs.get()
-    days = nomas_dienas.get()
+    subs = abonements.get()
+    training = trenins.get()
+    services = pakalpojums.get()
+    region = rajons.get()
 
-    with open("autonoma.txt", "a", encoding="utf-8") as file:
-        file.write(f"Veids: {auto_type}, Marka: {auto_mark}, Dzinējs: {engine}, Nomas dienas: {days}")
+    with open("sportazale.txt", "a", encoding="utf-8") as file:
+        file.write(f"Abonementa veids: {subs}, Treniņa veids: {training}, Pakalpojums: {services}, Rajons: {region}")
 
     messagebox.showinfo("Veiksmīgi, Dati saglabāti")
-    auto_veids.delete(0, END)
-    auto_markas.delete(0, END)
-    dzinejs.delete(0, END)
-    nomas_dienas.delete(0, END)
+    abonements.delete(0, END)
+    trenins.delete(0, END)
+    pakalpojums.delete(0, END)
+    rajons.delete(0, END)
 
 def aizvert_window():
     logs.destroy()
